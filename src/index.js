01d74fd1be03cf6dc7843692899d3ca63881808e
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-13 20:38:17 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // const input = document.querySelector("input#searchByID");
+    const inputValue = e.target.children[1].value;
+
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        const title = document.querySelector("section#movieDetails h4");
+        title.innerText = data.title;
+        const summary = document.querySelector("section#movieDetails p");
+        summary.innerText = data.summary;
+      });
+    //   .catch((error) => {
+    //     console.log("error", error);
+    //     const title = document.querySelector("section#movieDetails h4");
+    //     title.innerText = "No movie found";
+    //     const summary = document.querySelector("section#movieDetails p");
+    //     summary.innerText = "";
+    //   });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
